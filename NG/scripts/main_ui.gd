efdf156c1be1b02575c545a51fdd2860ece9ece3
HRQ-99 <harmanss1234@gmail.com>
2025-03-17 19:26:41 -0700

    Added assets & testing new nodes

---
+++ NG/scripts/main_ui.gd
@@ -0,0 +1,15 @@
+extends CanvasLayer
+
+func _on_start_button_pressed() -> void:
+  pass # Replace with function body.
+
+
+func _on_achievements_button_pressed() -> void:
+  pass # Replace with function body.
+
+
+func _on_options_button_pressed() -> void:
+  pass # Replace with function body.
+
+func _on_exit_button_pressed() -> void:
+  GameCleanup.new().exit_game()
---
+++ NG/utilities/game_cleanup.gd
@@ -0,0 +1,5 @@
+class_name GameCleanup
+
+func exit_game():
+  print("cleaning up")
+  Input.set_custom_mouse_cursor(null)
